3d88c9c358ecb9d95956eabc1c8edd8f64a6ce09
Jone <jone.work@outlook.com>
2020-12-18 11:33:44 +0800

    配置不同环境运行

--- lib/common/http/UrlsConfig.dart
+++ lib/common/http/UrlsConfig.dart
@@ -1,6 +1,4 @@
 class UrlsConfig {
-
-  static String API_HOST = "http://192.168.0.125:8985/";
-  // static String API_HOST = "http://api.suleyman.io";
-
+  static String API_HOST_DEV = "http://192.168.0.125:8985/";
+  static String API_HOST = "http://api.suleyman.io/";
 }
